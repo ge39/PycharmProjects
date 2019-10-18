d54967403da9b82961da7a381b6667f652ac771a
Jeremias <ge39.medeiros@gmail.com>
2019-10-18 16:42:11 -0300

    modulos e funçoes

--- Teste_Python/desafio_conv_temperatura.py
+++ Teste_Python/desafio_conv_temperatura.py
@@ -3,5 +3,5 @@
 # em °C e converta em°F
 
 temp = float(input('digite a temperatura em °C :'))
-f = ((9 * c) / 5) + 32
+f = ((9 * c / 5) + 32
 print('A temperatura de {0} °C corresponde a {1}F° '.format(temp,f))
--- Teste_Python/desafio_raizquadrada.py
+++ Teste_Python/desafio_raizquadrada.py
@@ -1,8 +1,10 @@
 # crie um algoritmo que leia um numerico e mostre
 # o seu dobro, triplo e raiz quadrada.
+from math import sqrt
 
 n1 = int(input('digite um numero'))
 dobro = int(n1 * 2)
 triplo = int(n1 * 3)
-raiz = (n1 **(1/2))
+#raiz = (n1 **(1/2))
+raiz = sqrt(n1)
 print("Numero digitado: {}\n O dobro de: {} é {}\n O triplo de: {} é {}\n Raiz Quadrada {}".format(n1, n1, dobro, n1, triplo,raiz))
