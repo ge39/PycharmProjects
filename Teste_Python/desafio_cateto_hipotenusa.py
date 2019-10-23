fc835c81df801dda69e184466537981c58c565d9
Jeremias <ge39.medeiros@gmail.com>
2019-10-23 19:53:42 -0300

    sorteando um aluno numa lista

--- Teste_Python/desafio_cateto_hipotenusa.py
+++ Teste_Python/desafio_cateto_hipotenusa.py
@@ -5,6 +5,6 @@ from math import hypot
 co = float(input('digite o cateto oposto :'))
 ca = float(input('digite o cateto adjacente :'))
 #h1 = (co ** 2 + ca ** 2)**(1/2) metodo convencional
-h1 = hypot(co,ca)
+h1 = hypot(co, ca)
 
 print("valor cateto oposto :{}\nvalor cateto adjacente :{}\ncomprimento da hipotenusa :{:.2f}".format(co, ca, h1))
