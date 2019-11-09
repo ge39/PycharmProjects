272d6c12ce084ae73e63fa0de42e8691eaf31282
Jeremias <ge39.medeiros@gmail.com>
2019-11-09 17:48:40 -0300

    estruturas condidicional 4,advinhacao

--- Teste_Python/desafio28_Jogo_advinhacao.py
+++ Teste_Python/desafio28_Jogo_advinhacao.py
@@ -1,13 +1,14 @@
 #escreva um programa que faça o computador pensar
 #entre um numero entre zero e cinco, e peca para o usuario tentar descobrir
 #qual foi o numero escolhido pelo computador.
-#o programa deverá escrever na tela se o usuário venceu ou perde
+#o programa deverá escrever na tela se o usuário venceu ou perdeu
 
 from random import choice
 lista = (0, 1, 2, 3, 4, 5)
 escolhido = choice(lista)
 
 numero = int(input('digite um numero :'))
+print('Processando....')
 if numero == escolhido:
     print('Parabens, o numero escolhido foi {}'.format(escolhido))
 else:
