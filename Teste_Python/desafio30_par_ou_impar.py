a5d7beb68786b2195a48b421fd01e0e4a92e8431
Jeremias <ge39.medeiros@gmail.com>
2019-11-09 18:16:03 -0300

    estruturas condidicional ,par ou impar

--- Teste_Python/desafio30_par_ou_impar.py
+++ Teste_Python/desafio30_par_ou_impar.py
@@ -1,1 +1,6 @@
 #crie um programa que leia um numero inteiro e mostre na tela se ele é par ou impar
+
+num = int(input('digite um numero :'))
+resultado = num % 2
+
+print('esse número é par' if resultado == 0 else 'esse numero é impar')
