ce39bfa9f9ff77ecc849c2095e581edd2502b137
Jeremias <ge39.medeiros@gmail.com>
2019-11-11 20:49:07 -0300

    estruturas condidicional ,cors no terminal

--- Teste_Python/desafio35_retas_do_triangulo.py
+++ Teste_Python/desafio35_retas_do_triangulo.py
@@ -8,6 +8,6 @@ r3 = float(input('terceiro segmento :'))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Os segmentos acima podem formar triangulos')
 else:
-    print('Os segmentos acima não podem formar triangulo')
+    print("Os segmentos acima não podem formar triangulo")
 
 
---
+++ Teste_Python/fase11-cores_no_terminal.py
@@ -0,0 +1,10 @@
+#ansi
+#escape sequence
+#style; text; background
+
+#style #0 none; #1 bold; #4 underline; #7 negative
+
+#text 30-branco;31-vermelho;33-amarelo;34-azul; 35-magenta; 36-ciano; 37-cinza
+#background 40-branco;41-vermelho;43-amarelo;44-azul; 45-magenta; 46-ciano; 47-cinza
+
+\033[0;33;44m
