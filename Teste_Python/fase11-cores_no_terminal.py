ef2fffa6eec92e9f4bd69950d114470444414225
Jeremias <ge39.medeiros@gmail.com>
2019-11-12 19:05:25 -0300

    estruturas condidicional ,cores no terminal

--- Teste_Python/fase11-cores_no_terminal.py
+++ Teste_Python/fase11-cores_no_terminal.py
@@ -1,10 +1,18 @@
-#ansi
-#escape sequence
-#style; text; background
+# ansi
+# escape sequence
+# style; text; background
 
-#style #0 none; #1 bold; #4 underline; #7 negative
+# style #0 none; #1 bold; #4 underline; #7 negative
 
-#text 30-branco;31-vermelho;33-amarelo;34-azul; 35-magenta; 36-ciano; 37-cinza
-#background 40-branco;41-vermelho;43-amarelo;44-azul; 45-magenta; 46-ciano; 47-cinza
+# text 30-branco;31-vermelho;33-amarelo;34-azul; 35-magenta; 36-ciano; 37-cinza
+# background 40-branco;41-vermelho;43-amarelo;44-azul; 45-magenta; 46-ciano; 47-cinza
 
-\033[0;33;44m
+print('\033[1;31;40mOlá Mundo\033[m')
+a = 3
+b = 5
+
+print('Os valores são: \033[33m{}\033[m e \033[31m{}\033[m'.format(a, b))
+
+print('O valor é: \033[31m {}\033[m a outro valor é \033[34m {}\033[m'.format(a, b))
+print('\033[34m next\033[m -' * 20)
+print('\033[0;30;45m Os valores sao: {}\033[m'.format(a))
