eabeddf8248df19c4f7751e9db4a47c2bf778082
Jeremias <ge39.medeiros@gmail.com>
2019-11-13 19:01:47 -0300

    criado diretorio Python-Mundo2 e alterado diretorio Teste_python para Python-Mundo1

--- Python-Mundo1/Scripts/pip-script.py
+++ Python-Mundo1/Scripts/pip-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\Jeremias\Desktop\Teste_Python\Scripts\python.exe
+#!C:\Users\Jeremias\Desktop\Python-Mundo1\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip'
 __requires__ = 'pip==19.0.3'
 import re
