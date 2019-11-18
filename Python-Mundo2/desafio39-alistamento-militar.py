59f6579bf5a262735629f63ff1cbac046c8a225c
Jeremias <ge39.medeiros@gmail.com>
2019-11-18 19:21:07 -0300

    desafio38 - alistamento militar

---
+++ Python-Mundo2/desafio39-alistamento-militar.py
@@ -0,0 +1,7 @@
+#faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade:
+
+#Se ele ainda vai se alistar ao servico militar
+#se é hora de se alistar
+#se ja passou da hora de se alistar
+
+#seu programa deve mostrar tambem o tempo que falta ou que passou do prazo
