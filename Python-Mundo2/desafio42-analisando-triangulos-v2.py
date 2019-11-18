224dd2f6ac02f6d5854ba086d8e877d065a80052
Jeremias <ge39.medeiros@gmail.com>
2019-11-18 20:19:20 -0300

    desafio40 - Media das provas

---
+++ Python-Mundo2/desafio42-analisando-triangulos-v2.py
@@ -0,0 +1,5 @@
+#refaça o desafio 35 dos triangulos,acrescentando o recurso de mostrar que tipo de triangulo será formado
+
+#equilátero: todos os lados iguais
+#isoceles: dois lados iguais
+#escaleno: todos os lados diferentes
---
+++ Python-Mundo2/desafio44-gerenciador-de-pagamentos.py
@@ -0,0 +1,7 @@
+#elabore um programa que calcule o valor a ser pago por um produto, considerando o seu
+#preço normal e condição de pagamento:
+
+#- a vista dinheiro/cheque: 10% de desconto
+#- a vista no cartao: 5% de desconto
+#- em até 2 vezes no cartao: preço normal
+#- 3 vezes ou mais no cartao: 20% de juros
