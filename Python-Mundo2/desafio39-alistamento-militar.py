379cb6b566ffa495872f6da9f9a440191f63b4ce
Jeremias <ge39.medeiros@gmail.com>
2019-11-18 20:06:24 -0300

    desafio39 - alistamento militar

--- Python-Mundo2/desafio39-alistamento-militar.py
+++ Python-Mundo2/desafio39-alistamento-militar.py
@@ -5,3 +5,19 @@
 #se ja passou da hora de se alistar
 
 #seu programa deve mostrar tambem o tempo que falta ou que passou do prazo
+
+from datetime import date
+idade = int(input('Qual sua idade :'))
+ano = date.today().year
+nascido = ano -idade
+print('Voce nasceu no ano de {}'.format(nascido))
+anoalistar = ano - 18
+print('\033[33mAlistamento para nascidos no ano de {}\033[m'.format(anoalistar))
+
+if anoalistar == nascido:
+        print('\033[34mVoce tem {} anos e está apto ao serviço militar\033[m'.format(idade))
+elif anoalistar > nascido:
+    print('Voce tem {} anos e está atrasado em {} anos '.format(idade, anoalistar-nascido))
+else:
+    print('\033[31mVoce tem {} anos, e precisa esperar mais {} ano\033[m'.format(idade,nascido-anoalistar))
+
