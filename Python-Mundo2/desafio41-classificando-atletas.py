296beac3b1dc35cfb128f548a9b6de4696d975d4
Jeremias <ge39.medeiros@gmail.com>
2019-11-19 19:41:14 -0300

    desafio41 - Classificação de atletas

--- Python-Mundo2/desafio41-classificando-atletas.py
+++ Python-Mundo2/desafio41-classificando-atletas.py
@@ -7,4 +7,18 @@
 #-até 20 anos: senior
 #-acima: master
 
-ano = int(input('Ano que nasceu'))
+from datetime import date
+ano = date.today().year
+nasc = int(input('Ano que nasceu :'))
+result = ano - nasc
+
+if result < 10:
+    print('voce tem {} anos, sua categoria é Mirim'.format(result))
+elif 9 < result and result < 15:
+    print('voce te {} anos, sua categoria é Infantil'.format(result))
+elif result <= 19 and result > 14:
+    print('Voce tem {} anos, Sua categoria é Junior'.format(result))
+elif result > 19 and result <= 20:
+    print('Voce tem {} anos, sua categoria é Senior'.format(result))
+else:
+    print('Voce tem {} anos, sua categoria é Master'.format(result))
