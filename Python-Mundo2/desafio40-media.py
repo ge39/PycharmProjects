e8baebd35db4535214cd620865fffe44c565b2d6
Jeremias <ge39.medeiros@gmail.com>
2019-11-19 19:10:11 -0300

    desafio40 - Média das notas

--- Python-Mundo2/desafio40-media.py
+++ Python-Mundo2/desafio40-media.py
@@ -8,5 +8,14 @@
 #média 7.0 ou superior
 #aprovado
 
-nota1 = float(input('Primeira nota'))
-nota2 = float(input('Segunda nota'))
+n1 = float(input('Primeira nota :'))
+n2 = float(input('Segunda nota :'))
+result = (n1 + n2)/2
+if result < 5:
+    print('Sua média é\033[31m {}\033[m, \033[31mVocê foi reprovado\033[m'.format(result))
+elif result == 5 or result <= 6.9:
+    print('Sua média é \033[32m{}\033[m , Voce está de \033[32mrecuperação\033[m'.format(result))
+else:
+    print('\033[32mAprovado - \033[m' * 5, sep='-',end ='')
+    print('\033[32mAprovado\033[m')
+    print('Sua média é {}, Voce está aprovado'.format(result))
