dd07509937c10f95d417da4f1f7da9806541190b
ge39 <ge39.medeiros@gmail.com>
2019-11-20 12:11:42 -0300

    desafio43 - calculo IMC

--- Python-Mundo2/desafio43-imc.py
+++ Python-Mundo2/desafio43-imc.py
@@ -6,3 +6,22 @@
 #25 até 30: sobrepeso
 #30 até 40: obesidade
 #acima de 40: obesidade morbida
+
+peso = float(input('Qual é o seu Peso (kg) :'))
+altura = float(input('Sua altura (m) :'))
+
+imc = (peso / (altura ** 2))
+print('O imc dessa pessoa é {:.2f}'.format(imc), end=" ")
+
+if imc < 18.5:
+    print('e está abaixo do peso ideal')
+elif 18.5 <= imc <= 25:
+    print('e está no peso ideal')
+elif 25 <= imc < 30:
+    print('e está com sobrepeso')
+elif 30 <= imc < 40:
+    print('e está obeso')
+else:
+    print('e está com obesidade morbida, cuidado')
+
+
