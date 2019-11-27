61d9d79cbc82a9719371e246f1df5c6d8e183047
ge39 <ge39.medeiros@gmail.com>
2019-11-27 10:28:18 -0300

    desafio42 -Analisando e classificando triangulos

--- Python-Mundo2/desafio42-analisando-triangulos-v2.py
+++ Python-Mundo2/desafio42-analisando-triangulos-v2.py
@@ -9,12 +9,12 @@ r2 = float(input('Segundo segmento :'))
 r3 = float(input('Terceiro segmento :'))
 
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('segmento forma o triangulo', end="")
+    print('\033[34msegmento forma um triangulo ', end="")
     if r1 == r2 == r3:
-        print('EQUILATERO')
+        print('EQUILATERO!')
     elif r1 != r2 != r3 != r1:
-        print('ESCALENO')
+        print('ESCALENO!')
     else:
-        print('ISOCELES')
+        print('ISOCELES!')
 else:
     print('Esses segmentos nao formam um triangulo')
