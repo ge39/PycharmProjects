cfb474f891dfeeddcf1278a9d4ee24007c18038b
ge39 <ge39.medeiros@gmail.com>
2019-11-27 09:33:36 -0300

    desafio39 - Alistamento militar

--- Python-Mundo2/desafio39-alistamento-militar.py
+++ Python-Mundo2/desafio39-alistamento-militar.py
@@ -7,17 +7,18 @@
 #seu programa deve mostrar tambem o tempo que falta ou que passou do prazo
 
 from datetime import date
-idade = int(input('Qual sua idade :'))
-ano = date.today().year
-nascido = ano -idade
-print('Voce nasceu no ano de {}'.format(nascido))
-anoalistar = ano - 18
-print('\033[33mAlistamento para nascidos no ano de {}\033[m'.format(anoalistar))
+ano = int(input('Qual ano que voce nasceu? :'))
+ano_atual = date.today().year
+idade = (ano_atual -ano)
+print('Voce tem {} anos  em {}'.format(idade, ano_atual))
 
-if anoalistar == nascido:
-        print('\033[34mVoce tem {} anos e está apto ao serviço militar\033[m'.format(idade))
-elif anoalistar > nascido:
-    print('Voce tem {} anos e está atrasado em {} anos '.format(idade, anoalistar-nascido))
+
+print('\033[33mAlistamento para nascidos no ano de {}\033[m'.format(ano_atual))
+
+if idade == 18:
+        print('\033[34mVoce tem {} anos em {} e está apto ao serviço militar\033[m'.format(idade, ano_atual))
+elif idade > 18:
+    print('Voce tem {} anos em {} e está atrasado em {} anos '.format(idade, ano_atual, idade - 18))
 else:
-    print('\033[31mVoce tem {} anos, e precisa esperar mais {} ano\033[m'.format(idade,nascido-anoalistar))
+    print('\033[31mVoce tem {} anos em {} , e precisa esperar mais {} ano\033[m'.format(idade, ano_atual, 18 - idade))
 
