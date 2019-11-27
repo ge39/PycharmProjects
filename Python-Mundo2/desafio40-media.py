00844f308d669c17e6887939a16b07be40e12bd6
ge39 <ge39.medeiros@gmail.com>
2019-11-27 10:03:25 -0300

    desafio40 - Media aluno

--- Python-Mundo2/desafio40-media.py
+++ Python-Mundo2/desafio40-media.py
@@ -1,21 +1,22 @@
-#crie um programa que leia duas notas de um aluno e calcule sua média,
-# mostrando uma mensagem no final, e acordo com a média atingida
+#  crie um programa que leia duas notas de um aluno e calcule sua média,
+#  mostrando uma mensagem no final, e acordo com a média atingida
 
-#-media abaixo de 5.0:
-#reprovado
-#média entre 5.0 e 6.9:
-#recuperação
-#média 7.0 ou superior
-#aprovado
+#  -media abaixo de 5.0:
+#  reprovado
+#  média entre 5.0 e 6.9:
+#  recuperação
+#  média 7.0 ou superior
+#  aprovado
 
 n1 = float(input('Primeira nota :'))
 n2 = float(input('Segunda nota :'))
-result = (n1 + n2)/2
-if result < 5:
-    print('Sua média é\033[31m {}\033[m, \033[31mVocê foi reprovado\033[m'.format(result))
-elif result == 5 or result <= 6.9:
-    print('Sua média é \033[32m{}\033[m , Voce está de \033[32mrecuperação\033[m'.format(result))
+media = (n1 + n2)/2
+print('Tirando {:.1f} e {:.1f}, a média do aluno é {:.1f}'.format(n1, n2, media))
+if media < 5:
+    print('Sua média é\033[31m {}\033[m, \033[31mVocê foi reprovado\033[m'.format(media))
+elif 7 > media >= 5:
+    print('Sua média é \033[32m{}\033[m , Voce está de \033[32mrecuperação\033[m'.format(media))
 else:
     print('\033[32mAprovado - \033[m' * 5, sep='-', end='')
     print('\033[32mAprovado\033[m')
-    print('Sua média é {}, Voce está aprovado'.format(result))
+    print('Sua média é {}, Voce está aprovado'.format(media))
