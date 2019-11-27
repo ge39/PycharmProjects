5f68c7b71f896c10986d9bcea425e626f9a24cca
ge39 <ge39.medeiros@gmail.com>
2019-11-27 10:16:10 -0300

    desafio41 - Classifcação atleta por idade

--- Python-Mundo2/desafio41-classificando-atletas.py
+++ Python-Mundo2/desafio41-classificando-atletas.py
@@ -1,24 +1,24 @@
 #a confederação nacional de natação precisa de um programa que leia o ano de nascimento de um atleta e  mostre
 #e mostre sua categoria, de acordo com a idade:
 
-#-até 9 anos mirim
-#-até 14 anos:infantil
-#-até 19 anos: junior
-#-até 20 anos: senior
-#-acima: master
+#  -até 9 anos mirim
+#  -até 14 anos:infantil
+#  -até 19 anos: junior
+#  -até 20 anos: senior
+#  -acima: master
 
 from datetime import date
 ano = date.today().year
 nasc = int(input('Ano que nasceu :'))
-result = ano - nasc
-
-if result < 10:
-    print('voce tem {} anos, sua categoria é Mirim'.format(result))
-elif 9 < result and result < 15:
-    print('voce te {} anos, sua categoria é Infantil'.format(result))
-elif result <= 19 and result > 14:
-    print('Voce tem {} anos, Sua categoria é Junior'.format(result))
-elif result > 19 and result <= 20:
-    print('Voce tem {} anos, sua categoria é Senior'.format(result))
+idade = ano - nasc
+print('O atleta tem {} anos'.format(idade))
+if idade < 9:
+    print('Categoria: Mirim'.format(idade))
+elif idade <= 14:
+    print('Categoria: Infantil'.format(idade))
+elif idade <= 19:
+    print('Categoria: Junior'.format(idade))
+elif idade <= 25:
+    print('Categoria: Senior'.format(idade))
 else:
-    print('Voce tem {} anos, sua categoria é Master'.format(result))
+    print('Categoria: Master'.format(idade))
