fa78350c590c6f054e4dbd146ee51ae0f720f7ae
Jeremias <ge39.medeiros@gmail.com>
2019-11-28 20:36:48 -0300

    Aula13- desafio49 - Tabuada dinâmica

--- Python-Mundo3/desafio49.py
+++ Python-Mundo3/desafio49.py
@@ -1,2 +1,6 @@
 #  refaça o desafio 009, mostrando a tabuada de um numero que o usuario escolher.
 #  só que agora utilizarei um laço for
+
+num = int(input('Digite a tabuada :'))
+for c in range(1, 11,):
+    print(num, 'x', c, '= {}'.format( c * num))
