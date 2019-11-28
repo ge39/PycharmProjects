fbbd4104a5a20f5ee704451fef9d05434ae58bed
Jeremias <ge39.medeiros@gmail.com>
2019-11-28 19:02:59 -0300

    aula13-exercicio1 - estrutura de repeticao

--- Python-Mundo3/Aula13-exercico1.py
+++ Python-Mundo3/Aula13-exercico1.py
@@ -1,1 +1,9 @@
 # estrutura de repetição ou iteração
+
+i = int(input('\033[34mInicio :'))
+f = int(input('Fim :'))
+p = int(input('Passo :\033[m'))
+
+for c in range(i, f, p):
+    print(c, sep="-",end='-')
+print('Fim')
