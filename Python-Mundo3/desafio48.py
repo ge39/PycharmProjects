d7de67e7ada9f8981fc7900df20536558e56e756
Jeremias <ge39.medeiros@gmail.com>
2019-11-28 20:25:48 -0300

    Aula13- desafio48 - soma numeros multiplos de tres

--- Python-Mundo3/desafio48.py
+++ Python-Mundo3/desafio48.py
@@ -1,2 +1,8 @@
 #  faca um programa que calcule a soma entre todos numeros impares que sao multiplos de tres
 #  e que se encontram no  intervalo de 1 até 500
+soma = 0
+for c in range(1, 500, 2):
+    soma = (soma + c)
+
+    print(c, end='-')
+print('\n\033[34mA soma de todos os numeros é {}\033[m'.format(soma))
