011d97b0d10f41392991f771c1a405f28761938d
ge39 <ge39.medeiros@gmail.com>
2019-11-29 10:27:25 -0300

    Merge branch 'develop' of https://github.com/ge39/Projetos_Python into develop
    # Conflicts:
    #	Python-Mundo2/desafio44-gerenciador-de-pagamentos.py

--- Python-Mundo3/desafio50.py
+++ Python-Mundo3/desafio50.py
@@ -1,2 +1,9 @@
 #  desenvolva um sistema que leia seis numeros inteiros e mostre a soma apenas daqueles que forem pares.
 #  se o valor digitado for impar, desconsidere-o
+for c in range(1, 7):
+    var = c
+    num = int(input('Digite o {}º numero:'.format(var)))
+soma = 0
+for n1 in range(0, 7, 2):
+    soma += n1
+print(soma)
