390ed07a2402d2f2f0b744b9db54047a0c1b4d4d
ge39 <ge39.medeiros@gmail.com>
2019-11-29 09:50:27 -0300

    desafio44 -Gerenciador de pagamento

--- Python-Mundo2/desafio44-gerenciador-de-pagamentos.py
+++ Python-Mundo2/desafio44-gerenciador-de-pagamentos.py
@@ -6,12 +6,12 @@
 # 3- em até 2 vezes no cartao: preço normal
 # 4- 3 vezes ou mais no cartao: 20% de juros
 
-print('\033[31mForma de pagamento\033[m')
+print('\033[31m{:=^40}\033[m'.format(' Lojas Guanabara '))
 print('\033[34m\n'
       '    1 - DINHEIRO / CHEQUE\n'
       '    2 - CARTÃO DÉBTO\n'
       '    3 - CARTÃO 2 VEZES\n'
-      '    4 - CARTÃO ACIMA DE 2 VEZES\033[m')
+      '    4 - 3 X OU MAIS NO CARTÃO\033[m')
 print('=' * 100)
 valor = float(input('Digite o valor da compra R$ '))
 pagto = int(input('Digite a forma de pagamento :'))
