b39f07411cbd653f4dc655845eda6b4e88f63a0f
Jeremias <ge39.medeiros@gmail.com>
2019-11-29 17:55:02 -0300

    Aula13- desafio51 - Termo de uma razao

--- Python-Mundo3/desafio51.py
+++ Python-Mundo3/desafio51.py
@@ -1,2 +1,3 @@
 #  desenvolva um programa que leia o primeiro termo e a razão de uma PA.
-#  no final, mostre os 10 primeiros termos dessa progressão
+#  no final, mostre os 10 primeiros termos dessa progressão.
+
