7b296af8b5f9c84afa3b3fb0437ff8e50ecfd099
Jeremias <ge39.medeiros@gmail.com>
2019-11-30 18:59:07 -0300

    Aula13- desafio49 - Tabuada dinamica

--- Python-Mundo3/desafio50.py
+++ Python-Mundo3/desafio50.py
@@ -1,9 +1,11 @@
 #  desenvolva um sistema que leia seis numeros inteiros e mostre a soma apenas daqueles que forem pares.
 #  se o valor digitado for impar, desconsidere-o
+soma = 0
 for c in range(1, 7):
     var = c
     num = int(input('Digite o {}º numero:'.format(var)))
-soma = 0
-for n1 in range(0, 7, 2):
-    soma += n1
-print(soma)
+
+    if num % 2 == 0:
+        #print(num)
+        soma += num
+print('Soma dos numero pares são {}'.format(soma))
