7db643e63bd5d089928c0eca2a2f134433969988
Jeremias <ge39.medeiros@gmail.com>
2019-11-30 19:19:50 -0300

    Aula13- desafio50 - Soma somente os numeros pares

--- Python-Mundo3/desafio50.py
+++ Python-Mundo3/desafio50.py
@@ -1,12 +1,10 @@
 #  desenvolva um sistema que leia seis numeros inteiros e mostre a soma apenas daqueles que forem pares.
 #  se o valor digitado for impar, desconsidere-o
 soma = 0
+cont = 0
 for c in range(1, 7):
-    var = c
-    num = int(input('Digite o {}º numero:'.format(var)))
-
+    num = int(input('Digite o {}º numero:'.format(c)))
     if num % 2 == 0:
-        # print(num)
-        soma += num
-print('Soma dos numero pares são {}'.format(soma))
-#  linha
+       soma += num
+       cont = cont + 1
+print('Total dos numeros pares {}, e sua soma é {}'.format(cont, soma))
