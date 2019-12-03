2d431693cc38959aaeaec2999b60db59620390e9
Jeremias <ge39.medeiros@gmail.com>
2019-12-03 20:51:32 -0300

    Aula13- desafio55 - Menor e maior peso

--- Python-Mundo3/desafio56.py
+++ Python-Mundo3/desafio56.py
@@ -3,3 +3,9 @@
 #  a media de idade do grupo
 #  o nome do homem mais velho
 #  quantas mulheres tem menos de 20 anos
+
+for p in (1, 5):
+    print('======={}ª Pessoa=======')
+    nome = str(input('Nome da {}ª Pessoa :'.format(p)))
+    idade = int(input('idade da {}ª pessoa'.format(p)))
+    sexo = str(input('Sexo da {}ª pessoa [M/F]'.format(p))).strip()
