aa288d0b68401c33d87b2f92cd8a16b098d842e4
Jeremias <ge39.medeiros@gmail.com>
2019-12-03 20:30:46 -0300

    Aula13- desafio55 - Menor e maior peso

--- Python-Mundo3/desafio55.py
+++ Python-Mundo3/desafio55.py
@@ -1,6 +1,17 @@
 #  faça um programa que leia o peso de 5 pessoas.
 #  no final, mostre qual foi o maior e menor peso lidos
 
-
+maior = 0
+menor = 0
 for p in range(1, 6):
-    peso = float(input('Pessoa {}ª - Digite seu peso'.format(p)))
+    peso = float(input('Peso da {}ª pessoa :'.format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido foi de {}Kg'.format(maior))
+print('O menor peso lido foi de {}kg'.format(menor))
