c1af74837db8bbdca12e04b7b1601f61a1709879
Jeremias <ge39.medeiros@gmail.com>
2019-12-03 20:14:35 -0300

    Aula13- desafio54 - Estatisticas de maioridade

--- Python-Mundo3/desafio54.py
+++ Python-Mundo3/desafio54.py
@@ -1,15 +1,17 @@
 #  crie um programa que leia o ano de nascimento de 07 pessoas.
 #  no final, mostre quantas pessoas ainda não atingiram a maioridade e quantas ja são maiores
 from datetime import date
-soma = 0
+totmaior = 0
+totmenor = 0
+atual = date.today().year
+
 for c in range(1, 8):
     nasc = int(input(' {}º Digite o ano de nascimento :'.format(c)))
-    atual = date.today().year
-    maioridade = atual - nasc
-    #print(maioridade)
-    if maioridade >=21:
-        print('Voce tem {} anos e é maior de 21 anos'.format(maioridade))
-        soma = soma + 1
-        print(soma)
-    #else:
-        #print('Você tem {} anos e não alcançou a maioridade'.format(maioridade))
+    idade = atual - nasc
+
+    if idade >= 21:
+        totmaior += 1
+    else:
+        totmenor +=1
+print('Você tem {} pessoas maiores de 21 anos'.format(totmaior))
+print('Você tem {} pessoas menores de 21 anos'.format(totmenor))
