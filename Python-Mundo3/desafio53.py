1b13ac99da3a30ba53d912e85560e87643ea2c64
Jeremias <ge39.medeiros@gmail.com>
2019-12-03 19:47:17 -0300

    Aula13- desafio53 -Palindromo

--- Python-Mundo3/desafio53.py
+++ Python-Mundo3/desafio53.py
@@ -1,4 +1,17 @@
 #  crie um programa que leia uma frase qualquer e diga se ele é um palindromo,
 #  desconsiderando os espacos
 
-frase = str(input('a sacada da casa')).strip()
+frase = str(input('Digite uma frase')).strip().upper()
+palavras = frase.split()    #divide a frase
+junto = ''.join(palavras)   # remove os espaços entre as palavras e as une
+inverso = junto[::-1]
+
+'''inverso = ''
+for letra in range(len(junto) - 1, -1, -1):
+    inverso += junto[letra]'''
+
+print('A frase escrita {}, {} '.format(inverso, junto))
+if inverso == junto:
+    print('Essa frase é um Palíndromo')
+else:
+    print('Essa frase não é um Palindromo')
