0f2a5d3833a1608a46fad32f5565a5f0710a74f4
Jeremias <ge39.medeiros@gmail.com>
2019-12-04 20:01:01 -0300

    Aula13- desafio56 - Soma de idades, média delas, o mais velho

--- Python-Mundo3/desafio56.py
+++ Python-Mundo3/desafio56.py
@@ -3,9 +3,28 @@
 #  a media de idade do grupo
 #  o nome do homem mais velho
 #  quantas mulheres tem menos de 20 anos
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
 
-for p in (1, 5):
-    print('======={}ª Pessoa=======')
-    nome = str(input('Nome da {}ª Pessoa :'.format(p)))
-    idade = int(input('idade da {}ª pessoa'.format(p)))
-    sexo = str(input('Sexo da {}ª pessoa [M/F]'.format(p))).strip()
+for p in range(1, 5):
+    print(f'========= {p}ª Pessoa =======')
+    nome = str(input('Nome   da {}ª Pessoa :'.format(p))).strip().upper()
+    idade = int(input('idade  da {}ª pessoa :'.format(p)))
+    sexo = str(input('Sexo   da {}ª pessoa [M/F] :'.format(p))).strip().upper()
+    print('')
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1
+mediaidade = somaidade / 4
+print('A média da idade do grupo {:.2f}'.format(mediaidade))
+print('O homem mais velho tem {} anos e se chama {}'.format(maioridadehomem,nomevelho))
+print('Ao todo são {} o total de mulheres com menos de 20 anos'.format(totmulher20))
