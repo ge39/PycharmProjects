3ae2008f21294aab4d6b53a9f74761140e3e511e
Jeremias <ge39.medeiros@gmail.com>
2019-12-10 19:20:49 -0300

    desafio 57- Validando o tipo do sexo

--- Python-Mundo2-P2/desafio57.py
+++ Python-Mundo2-P2/desafio57.py
@@ -4,3 +4,11 @@ desafio57
 	mas só aceite os valores M ou F.
 	caso esteja errado, peça a digitação novamente até ter um valor correto
 '''
+
+sexo = str(input('Digite seu sexo :')).strip().upper()
+while  'M'!= sexo !='F' :
+    sexo = str(input('Digite Novamente o sexo :')).upper().strip()
+else:
+    print ('sexo ok')
+
+
