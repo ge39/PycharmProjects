74ec5142f14deefb5cc018c1042a8b6eaba56368
Jeremias <ge39.medeiros@gmail.com>
2019-12-10 18:11:48 -0300

    desafio 58- Jogo adivinhação

---
+++ Python-Mundo2-P2/desafio58.py
@@ -0,0 +1,6 @@
+'''
+    desafio 58
+	Melhore o jogo do desafio 28, onde o computador vai 'PENSAR', em um numero entre 0 e 10.
+	só que agora o jogador vai tentar adivinhar até acertar. mostrando no final quantos
+	palpites foram necessarios para vencer
+'''
