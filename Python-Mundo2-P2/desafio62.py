3f6686b6603d116c0a7cd8b33958a66bc3e1dd76
Jeremias <ge39.medeiros@gmail.com>
2019-12-10 18:46:58 -0300

    desafio 62- Termo de uma progressão aritmética

--- Python-Mundo2-P2/desafio62.py
+++ Python-Mundo2-P2/desafio62.py
@@ -1,5 +1,6 @@
 '''
     desafio 62
 	Melhore o desafio 61, perguntando para o usuario se ele quer mostrar mais alguns termos.
-	O programa encerra quando ele disser que quer mostrar.0 termos
+	O programa encerra quando ele disser que quer mostrar.0 termo
+
 '''
