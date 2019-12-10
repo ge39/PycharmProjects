5980547d4c6e329788beec8392ebbadf69a9a6aa
Jeremias <ge39.medeiros@gmail.com>
2019-12-10 18:19:49 -0300

    desafio 59- Lendo 2 valores

---
+++ Python-Mundo2-P2/desafio59.py
@@ -0,0 +1,11 @@
+'''
+    desafio 59
+	crie um programa que leia 2 valores e mostre um menu na tela:
+	[1] somar
+	[2] multiplicar
+	[3] maior
+	[4] novos numeros
+	[5] sair do programa
+
+	seu programa deverá realizar a operação solicitada em cada caso.
+'''
