db76f5df49e114f57d8b31928250432933ed2fc1
Jeremias <ge39.medeiros@gmail.com>
2019-12-10 20:23:54 -0300

    desafio 59- Lendo 2 valores e escolhendo operaçoes

--- Python-Mundo2-P2/desafio59.py
+++ Python-Mundo2-P2/desafio59.py
@@ -9,3 +9,15 @@
 
 	seu programa deverá realizar a operação solicitada em cada caso.
 '''
+num1 = int(input('Digite o Primeiro numero :'))
+num2 = int(input('Digite o Segundo numero :'))
+
+print ('''
+    [1] somar
+	[2] multiplicar
+	[3] maior
+	[4] novos numeros
+	[5] sair do programa
+
+''')
+
