e741c79283d6e9f870229af692626ea223e8f3cf
Jeremias <ge39.medeiros@gmail.com>
2019-12-11 19:53:25 -0300

    desafio57- validação de dados do sexo

--- Python-Mundo2-P2/desafio57.py
+++ Python-Mundo2-P2/desafio57.py
@@ -5,13 +5,13 @@ desafio57
 	caso esteja errado, peça a digitação novamente até ter um valor correto
 '''
 
-sexo = str(input('Digite seu sexo :')).strip().upper()
-while 'M' != sexo != 'F':
+sexo = str(input('Digite seu sexo :')).strip().upper()[0]# fatiando pra pegar só a primeira letra
+while sexo not in 'FfMm':   #while 'M' != sexo != 'F':
     sexo = str(input('Você errou, Digite novamente o sexo :')).upper().strip()
-
 if sexo == 'F':
     F = str('Feminino')
     print('Informação, {} para {} está correta'.format(sexo, F))
 else:
     M = str('Masculino')
     print('Informação {} para {} está correto'.format(sexo, M))
+
