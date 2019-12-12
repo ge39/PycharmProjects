cfa4f5670e06d7d755b27f340a0c47eff9f66be7
Jeremias <ge39.medeiros@gmail.com>
2019-12-12 20:23:43 -0300

    desafio59- Opção de acertos

--- Python-Mundo2-P2/desafio59.py
+++ Python-Mundo2-P2/desafio59.py
@@ -24,22 +24,22 @@ while num < 5:
     digite = int(input('\033[33mDigite um numero do Menu\033[m :'))
     if digite == 1:
         print(num1 + num2)
-    if digite == 2:
+    elif digite == 2:
         print(num1 * num2)
-    if digite == 3:
+    elif digite == 3:
         if num1 > num2:
             print('O valor \033[32m{}\033[m é maior que o valor \033[35m{}\033[m'.format(num1, num2))
         elif num1 < num2:
-            print('O valor {} é maior que o valor {}'.format(num2, num1))
+            print('O valor \033[32m{}\033[m é menor que o valor \033[35m{}\033[m'.format(num1, num2))
         else:
             print('Os  dois numeros \033[32m{} e {}\033[m são iguais'.format(num1, num2))
-    if digite == 4:
+    elif digite == 4:
         num1 = int(input('\033[31mDigite o Primeiro numero\033[m :'))
         num2 = int(input('\033[31mDigite o Segundo numero\033[m :'))
-    if digite == 5:
+    elif digite == 5:
         print('Fim')
         break
-    if digite > 5:
+    elif digite > 5:
         print('Opção incorreta')
         num1 = int(input('\033[31mDigite o Primeiro numero\033[m :'))
         num2 = int(input('\033[31mDigite o Segundo numero\033[m :'))
