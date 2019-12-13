c1952b79e6e934fada131fbac3c154ccb523a1ff
Jeremias <ge39.medeiros@gmail.com>
2019-12-13 20:06:35 -0300

    desafio60- Opção de acertos

--- Python-Mundo2-P2/desafio60.py
+++ Python-Mundo2-P2/desafio60.py
@@ -3,10 +3,21 @@
 	Faça um programa que leia um numero qualquer e leia seu fatorial
 	ex: 5! = 5x4x3x2x1 = 120
 '''
-contar = 0
+from time import sleep
 n = int(input('Digite um Numero :'))
+par = 0
+impar = 0
 
-while n < 10:
-    contar += n
-    print(contar)
+while n != 0:
+    sleep(1)
+    n -= 1
+    if n % 2 != 0:
+        print(n)
+
+
+
+
+
+
+
 
