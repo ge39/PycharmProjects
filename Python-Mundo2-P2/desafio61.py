ec700944972004347f3fe4ddc0c5627295560832
ge39 <ge39.medeiros@gmail.com>
2019-12-15 11:48:16 -0300

    desafio-61 - Progressão aritmética V2.0

--- Python-Mundo2-P2/desafio61.py
+++ Python-Mundo2-P2/desafio61.py
@@ -3,3 +3,17 @@
 	Refaça  desafio 51, lendo o primeiro termo e a razão de uma PA.
 	mostrando os 10 progressão usando a estrutura while
 '''
+num1 = 0
+num2 = 0
+contador = 0
+
+
+num1 = int(input('Digite o Primeiro termo :'))
+razao= int(input('Digite a Razão da PA :'))
+termo = num1
+contador = 1
+while contador <= 10:
+    print('{} '.format(termo), end='- ')
+    contador += 1
+    termo += razao
+print('FIM')
