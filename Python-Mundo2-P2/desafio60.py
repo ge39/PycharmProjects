2ee4dbfeaf4d50de868db75149f782936d6cc427
ge39 <ge39.medeiros@gmail.com>
2019-12-15 09:35:42 -0300

    desafio-60 - Calculo do fatorial

--- Python-Mundo2-P2/desafio60.py
+++ Python-Mundo2-P2/desafio60.py
@@ -2,22 +2,23 @@
     desafio 60
 	Faça um programa que leia um numero qualquer e leia seu fatorial
 	ex: 5! = 5x4x3x2x1 = 120
+
+utilizando o modulo math
+
+from math import factorial
+n = int(input('Digite um numero :'))
+f = factorial(n)
+print('Fatorial de {} é {}'.format(n, f))
 '''
-from time import sleep
+
 n = int(input('Digite um Numero :'))
-par = 0
-impar = 0
-
-while n != 0:
-    sleep(1)
-    n -= 1
-    if n % 2 != 0:
-        print(n)
-print(n)
-
-
-
-
-
-
-
+c = n
+f = 1
+print('\033[34mCalculando o Fatorial {}!\033[m = '.format(n), end='')
+while c > 0:
+       print('{}'.format(c),end= '')
+       print(' X ' if c > 1 else ' = ' , end='')
+       f *= c
+       c -= 1
+print('\033[31m{}\033[m'.format(f))
+        #print( 'Fatorando {} - {} X {} = {}'.format( n, n,fator, n * fator))
