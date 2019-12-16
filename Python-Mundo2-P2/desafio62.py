e4db60f1911377f5e482ac1400fabc079fcbb3fe
ge39 <ge39.medeiros@gmail.com>
2019-12-16 09:52:07 -0300

    desafio-62 - Progressão aritmética com opção

--- Python-Mundo2-P2/desafio62.py
+++ Python-Mundo2-P2/desafio62.py
@@ -4,3 +4,28 @@
 	O programa encerra quando ele disser que quer mostrar.0 termo
 
 '''
+
+num = int(input('Digite um numero :'))
+razao = int(input('Digite a Razão :'))
+termo = num
+contador = 0
+continua = 'S'
+while continua in 'S':
+    while contador <= 10:
+        print('{}'.format(termo), end=" ")
+        contador += 1   # conta a quantidade de repeticao informada na variavel contador no laço while
+        termo += razao  # soma o valor fornecido na variavel razao fora do laco while
+
+    cont = str(input('\nDeseja lançar outro termo :'))
+    if continua in 'Ss':
+        num1 = int(input('Digite um numero :'))
+        razao1 = int(input('Digite a Razão :'))
+        termo1 = num1
+        contador1 = 0
+
+        print('{}'.format(termo1), end=" ")
+        contador1 += 1  # conta a quantidade de repeticao informada na variavel contador no laço while
+        termo1 += razao1  # soma o valor fornecido na variavel razao fora do laco while
+    else:
+        print('FIM')
+        break
