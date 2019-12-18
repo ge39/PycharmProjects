e367c7e5df9df7a444dc50999b25bed83e89fb83
Jeremias <ge39.medeiros@gmail.com>
2019-12-18 19:00:09 -0300

    desafio63- Sequência de fibonacci

--- Python-Mundo2-P2/desafio63.py
+++ Python-Mundo2-P2/desafio63.py
@@ -3,5 +3,25 @@
 	Escreva um prodgrama que leia um numero N inteiro qualquer e mostre na tela
 	Os N primeiros elementos de uma sequenca de  Fibonacci
 
-	ex:0 - 1 - 2 - 3 - 5 - 8
+	ex:0 -> 1 -> 2 -> 3 -> 5 -> 8
 '''
+print('-'*30)
+print('Sequencia de Fibonacci')
+print('-'*30)
+n = int(input('Quantos termos voce quer mostra ?'))
+t1 = 0
+t2 = 1
+
+print('-'*30)
+print('{} -> {}'.format(t1, t2), end='')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print('-> {}'.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+else:
+    print('\033[31m\nFIM\033[m')
+
+
