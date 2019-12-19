4a6b3f337e89e64440c4f236f0a81bccd5506cdf
Jeremias <ge39.medeiros@gmail.com>
2019-12-19 17:59:54 -0300

    desafio63- Sequência de fibonacci

--- Python-Mundo2-P2/desafio63.py
+++ Python-Mundo2-P2/desafio63.py
@@ -9,8 +9,8 @@ print('-'*30)
 print('Sequencia de Fibonacci')
 print('-'*30)
 n = int(input('Quantos termos voce quer mostra ?'))
-t1 = 0
-t2 = 1
+t1 = 0  #termo1
+t2 = 1  #termo2
 
 print('-'*30)
 print('{} -> {}'.format(t1, t2), end='')
@@ -18,8 +18,8 @@ cont = 3
 while cont <= n:
     t3 = t1 + t2
     print('-> {}'.format(t3), end='')
-    t1 = t2
-    t2 = t3
+    t1 = t2  # t1 recebe valor do t2
+    t2 = t3  # t3 recebe o valor do t2
     cont += 1
 else:
     print('\033[31m\nFIM\033[m')
