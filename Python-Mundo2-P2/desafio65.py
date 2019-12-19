b2b8122fd006bae36455fe2ab781efb54b10dd76
Jeremias <ge39.medeiros@gmail.com>
2019-12-19 19:28:54 -0300

    desafio65 - Maior e menor valores

--- Python-Mundo2-P2/desafio65.py
+++ Python-Mundo2-P2/desafio65.py
@@ -6,3 +6,14 @@
 
 	o programa deve perguntar ao usuario se ele quer ou nao continuar a digitar valores
 '''
+continuar = 'S'
+cont = n = media = maior = menor = 0
+
+while continuar == 'S':
+    n = int(input('Digite um numero : '))
+    continuar = str(input('Deseja continuar [S/sim] [N/não] :')).upper()
+    cont += 1
+    n += n
+    media = n/cont
+else:
+    print('\033[31mTotal de numeros {}\033[m \n\033[32mSoma dos valores {:.2f}\033[m \033[34m \ne a sua média {:.2f}\033[m'.format(cont, n, media))
