35ea1c6cb341360081bfb65424d26d5cac3611a3
Jeremias <ge39.medeiros@gmail.com>
2019-12-20 20:01:08 -0300

    desafio67

---
+++ Python-Mundo2-P2/desafio67.py
@@ -0,0 +1,5 @@
+'''
+    Faça um programa que mostre a tabuaada de varios numeros
+    Um de cada vez, para cada valor digitado pelo usuario.
+    O programa será interrompido quando numero solicitado for negativo
+'''
