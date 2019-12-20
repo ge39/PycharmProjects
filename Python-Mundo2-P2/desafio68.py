d6a5671e6bfdce360bd3bf6c7bda8386dd614c33
Jeremias <ge39.medeiros@gmail.com>
2019-12-20 20:03:39 -0300

    desafio68-Par ou impar

---
+++ Python-Mundo2-P2/desafio68.py
@@ -0,0 +1,6 @@
+'''
+    Faça um programa que jogue par ou impar om o computador
+    o jogo será interrompido quando o jogador perder
+    mostrando o total de vitórias consecutivas
+    que ele conquistou no final do jogo
+'''
