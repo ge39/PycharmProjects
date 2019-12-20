9ed107509b77d13db9998d7bc75ac71ad49e137e
Jeremias <ge39.medeiros@gmail.com>
2019-12-20 20:13:05 -0300

    desafio70- Estatistica de venda de produto

---
+++ Python-Mundo2-P2/desafio70.py
@@ -0,0 +1,8 @@
+'''
+    crie um programa que leia o nome e opreço de varios produtos
+    O programa  deverá perguntar se o usuario vai continuar, e no final mostre
+
+    A - Qual é o total gasto na compra
+    B - Quantos produtos custam mais de R$ 100,00
+    C - Qual o nome do produto mais barato
+'''
