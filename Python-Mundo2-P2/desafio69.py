5769b59e50113a631dcdd910d966a29da6b8a532
Jeremias <ge39.medeiros@gmail.com>
2019-12-20 20:09:49 -0300

    desafio69- Estatistica de cadastro de pessoas

---
+++ Python-Mundo2-P2/desafio69.py
@@ -0,0 +1,8 @@
+'''
+    CRIE UM PROGRAMA que leia a idade e sexo de várias pessoas
+    a cada pessoa cadastrada, o programa deverá perguntar seo usuario quer ou nao continuar
+    no final mostre.
+    A - quantas pessoas com mais de 18 anos
+    B - quantos homens foram cadastrados
+    C - quantas mulheres tem menos de 20 anos
+'''
