70090a658861cf74711bd4eb4976e19ffefe8127
Jeremias <ge39.medeiros@gmail.com>
2019-12-20 20:00:33 -0300

    desafio64

---
+++ Python-Mundo2-P2/desafio66-Aula15.py
@@ -0,0 +1,6 @@
+'''
+    crie um programa que leia varios numeros inteiros pelo teclado.
+    o programa só vai parar quando o usuário digitar o valor 999. que é é a condição de parada
+    no final, mostre quantos numeros foram digitados e qual foio a soma entre eles
+    (desconsiderando o flag)
+'''
