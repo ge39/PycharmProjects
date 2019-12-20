235fb87df0c0b228ae1c9e56c66692e434094966
Jeremias <ge39.medeiros@gmail.com>
2019-12-20 20:59:18 -0300

    desafio71- Simulador de caixa eletronica

--- Python-Mundo2-P2/desafio66-Aula15.py
+++ Python-Mundo2-P2/desafio66-Aula15.py
@@ -4,3 +4,8 @@
     no final, mostre quantos numeros foram digitados e qual foio a soma entre eles
     (desconsiderando o flag)
 '''
+c = 0
+while c != 999:
+    c = int(input('Digite um numero [999 para parar]:'))
+else:
+    print('Fim')
