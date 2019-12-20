ae4ea65e64c1f95fd54fa1b8d5509408b1b7b37f
Jeremias <ge39.medeiros@gmail.com>
2019-12-20 20:55:39 -0300

    desafio71- Simulador de caixa eletronica

--- Python-Mundo2-P2/desafio71.py
+++ Python-Mundo2-P2/desafio71.py
@@ -3,5 +3,4 @@
     No inicio, pergunte ao cliente qual o valor a ser sacado (numero inteiro)
     e o programa irá informar quantas cedulas de cada valor serão entrgues
     OBS: considere que o caixa possui cedulas R$ 50,00, R$ 20,00, R$ 10,00, R$ 5,00 e R$1,00
-
 '''
