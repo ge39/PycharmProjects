ba31b9e04f97f37dfe84bc942cb5d91930a7a108
Jeremias <ge39.medeiros@gmail.com>
2019-12-21 17:22:28 -0300

    desafio71- Varios numeros com flag

--- Python-Mundo2-P2/desafio66-Aula15.py
+++ Python-Mundo2-P2/desafio66-Aula15.py
@@ -1,11 +1,14 @@
 '''
     crie um programa que leia varios numeros inteiros pelo teclado.
-    o programa só vai parar quando o usuário digitar o valor 999. que é é a condição de parada
+    o programa só vai parar quando o usuário digitar o valor 999. que é a condição de parada
     no final, mostre quantos numeros foram digitados e qual foio a soma entre eles
     (desconsiderando o flag)
 '''
-c = 0
+c = soma = cont = 0
 while c != 999:
     c = int(input('Digite um numero [999 para parar]:'))
-else:
-    print('Fim')
+    if c != 999:
+        cont += 1
+        soma += c
+    else:
+        print(f'Numeros digitados foram {cont} e a soma é {soma}')
