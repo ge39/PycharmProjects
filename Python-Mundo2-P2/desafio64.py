e2372487aa43cd0fd5fe42c6dfc601a0945f514f
Jeremias <ge39.medeiros@gmail.com>
2019-12-23 19:04:22 -0300

    desafio66- Tratando varios valores

--- Python-Mundo2-P2/desafio64.py
+++ Python-Mundo2-P2/desafio64.py
@@ -8,7 +8,7 @@ num = cont = soma = 0
 while num != 999:
     num = int(input('Digite um numero [\033[31m999 para parar]\033[m :'))
     if num != 999:
-        cont += 1
-        soma += num
+        cont += 1  # acumula na variavel cont quantas vezes a repetição foi executado
+        soma += num  # soma todos os valores digitado na variavel num
 else:
         print(f'\033[34mVoce contou {cont} numeros e a soma deles são {soma}\033[m', end='')
