e055d09578345aa55c3e2e584158062f1271eae6
Jeremias <ge39.medeiros@gmail.com>
2019-12-23 20:35:28 -0300

    desafio68- Jogo do par ou impar

--- Python-Mundo2-P2/desafio68.py
+++ Python-Mundo2-P2/desafio68.py
@@ -4,3 +4,33 @@
     mostrando o total de vitórias consecutivas
     que ele conquistou no final do jogo
 '''
+
+from random import randint
+p = i = num = jogada = vitoria = 0
+computador = randint(0, 6)
+continuar = 'S'
+
+while continuar in 'sS':
+    num = int(input('Digite um numero [6] para finalizar:')[0])
+    acao = str(input('Voce quer par [p] ou impar[i] :'))
+    soma = computador + num
+    #  print(f'A soma de {num} e {computador} é {soma}')
+
+    if soma % 2 == 0:
+        vitoria += 1
+        if acao in 'Pp':
+            print(f'{soma} é par e voce Venceu ')
+            jogada += 1
+        else:
+            print(f'{soma} é par e voce Perdeu ')
+    if soma % 2 != 0:
+        vitoria += 1
+        if acao in 'Pp':
+            print(f'{soma} é impar e voce Perdeu')
+        else:
+            print(f' {soma} é impar e voce Venceu')
+            jogada +=1
+    continuar = str(input('Deseja Continuar [S] Sim e [N] Não :'))
+if continuar in 'Ss':
+    print(f'Total de jogadas {jogada} e voce venceu {vitoria} vez(es)')
+    print("FIM de JOGO")
