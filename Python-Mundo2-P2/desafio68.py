34c290958bd4728f11058f23c40dfa5d1765e147
ge39 <ge39.medeiros@gmail.com>
2019-12-31 09:39:52 -0300

    desafio68- Jogo do par ou impar

--- Python-Mundo2-P2/desafio68.py
+++ Python-Mundo2-P2/desafio68.py
@@ -6,12 +6,12 @@
 '''
 
 from random import randint
-p = i = num = jogada = vitoria = 0
+p = i = num = jogada = vitoria = derrota = 0
 computador = randint(0, 6)
 continuar = 'S'
 
 while continuar in 'sS':
-    num = int(input('Digite um numero [6] para finalizar:')[0])
+    num = int(input('Digite um numero :')[0])
     acao = str(input('Voce quer par [p] ou impar[i] :'))
     soma = computador + num
     #  print(f'A soma de {num} e {computador} é {soma}')
@@ -23,15 +23,26 @@ while continuar in 'sS':
             vitoria += 1
         else:
             print(f'{soma} é par e voce Perdeu ')
+            derrota +=1
         continuar = str(input('Deseja Continuar [S] Sim e [N] Não :'))
     if soma % 2 != 0:
         jogada += 1
         if acao in 'Pp':
             print(f'{soma} é impar e voce Perdeu')
+            derrota += 1
         else:
             print(f' {soma} é impar e voce Venceu')
             vitoria += 1
         continuar = str(input('Deseja Continuar [S] Sim e [N] Não :'))
     if continuar in 'nN':
-        print(f'\033[34mTotal de jogadas {jogada}\033[m\n\033[33mVitoria {vitoria} vez(es)\033[m')
+        print('*'*20)
+        print(f'\033[34mTotal de jogadas {jogada}\033[m\n\033[31mCOMPUTADOR: {derrota}\033[m\n\033[32mJOGADOR: {vitoria} \033[m')
         print("FIM de JOGO")
+
+        if vitoria > derrota:
+            print('\033[32mPARABENS, VOCE É O VENCEDOR !!!\033[m')
+        elif vitoria == derrota:
+         print('\033[32mHOUVE EMPATE !!\033[m')
+        else:
+            print('\033[31mO COMPUTADOR GANHOU !!\033[m')
+        print('*' * 20)
