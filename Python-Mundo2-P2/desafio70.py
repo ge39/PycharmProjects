387d8acff338cd636299a47caec6d534ead77099
ge39 <ge39.medeiros@gmail.com>
2020-01-01 20:23:44 -0300

    desafio70- calculo de varios produtos

--- Python-Mundo2-P2/desafio70.py
+++ Python-Mundo2-P2/desafio70.py
@@ -18,16 +18,15 @@ while pergunta in 'sS':
    maiscaro = valor
    maisbarato = valor
 
-if valor > 100.00:
-  qtde += 1
-  valor
-if valor > maiscaro:
-   maiscaro = valor
-if valor < maisbarato:
-    maisbarato = valor
-    desc
+   if valor > 100:
+        qtde += 1
+   if valor > maiscaro:
+        maiscaro = valor
+   if valor < maisbarato:
+        maisbarato = valor
+        desc
 else:
-    print('*'*60)
-    print(f'Valor total gasto R$ {total:.2f}')
-    print(f'Na cesta tem  {qtde} produtos , que custa {valor:.2f} e é o maior valor da cesta')
-    print(f'O produto mais barato é {desc}')
+        print('*'*60)
+        print(f'Valor total gasto R$ {total:.2f}')
+        print(f'Na cesta tem  {qtde} produtos , que custa mais de R$ 100.00')
+        print(f'O produto mais barato é {desc}')
