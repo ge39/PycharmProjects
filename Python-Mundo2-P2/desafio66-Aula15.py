1739841fe15238de4b4bdf09c861c1327417d9d6
ge39 <ge39.medeiros@gmail.com>
2020-01-02 12:04:47 -0300

    desafio66 - leia varios numeros inteiros pelo teclado

--- Python-Mundo2-P2/desafio66-Aula15.py
+++ Python-Mundo2-P2/desafio66-Aula15.py
@@ -4,11 +4,11 @@
     no final, mostre quantos numeros foram digitados e qual foio a soma entre eles
     (desconsiderando o flag)
 '''
-c = soma = cont = 0
-while c != 999:
-    c = int(input('Digite um numero [999 para parar]:'))
-    if c != 999:
-        soma += c
-        cont += 1
-else:
-    print(f'Numeros digitados foram {cont} numeros e sua soma é {soma}')
+num = soma = cont = 0
+while True:
+    num = int(input('Digite um numero [999 para parar]:'))
+    if num == 999:
+        break
+    soma += num
+    cont += 1
+print(f'Numeros digitados foram {cont} numeros e sua soma é {soma}')
