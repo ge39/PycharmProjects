a860b7e9a1ec0cb1502ef664b4d1e8a3c75afb39
Jeremias <ge39.medeiros@gmail.com>
2020-01-03 20:26:12 -0300

    desafio69 - Contagem de pessoas do sexo feminino e masculino

--- Python-Mundo2-P2/desafio69.py
+++ Python-Mundo2-P2/desafio69.py
@@ -7,14 +7,15 @@
     C - quantas mulheres tem menos de 20 anos
 '''
 
-iniciar = 'S'
 continuar = 'S'
 idade = contsexo = fem = masc = 0
 sexo = 'F'
-while continuar in 'sS':
+while True:
     idade = int(input('Qual é a sua idade: ?'))
     sexo = str(input('Informe o sexo [M] Masculino, [F] Feminino :')[0]).upper()
-    continuar = str(input('\033[32mDeseja continuar cadastrando: ?\033[m')).upper()
+    continuar = str(input('\033[32mDeseja continuar cadastrando: [S] SIm [N] Não ?\033[m')).upper()
+    if continuar != 'S':
+        break
     contsexo += 1
     print('*' * 60)
 
@@ -24,7 +25,6 @@ while continuar in 'sS':
          masc += 1
     if idade < 20 and sexo == 'F':
         fem += 1
-else:
-    print(f'Temos {contsexo} pessoas com mais de 18 anos')
-    print(f'Total de {masc} pessoas do sexo masculino')
-    print(f'Total de {fem} pessoas do sexo feminino com menos de 20  anos')
+print(f'Temos {contsexo} pessoas com mais de 18 anos')
+print(f'Total de {masc} pessoas do sexo masculino')
+print(f'Total de {fem} pessoas do sexo feminino com menos de 20  anos')
