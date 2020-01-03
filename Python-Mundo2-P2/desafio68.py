b8467ccae8c7896ade2292b32597d66ecf1d323e
Jeremias <ge39.medeiros@gmail.com>
2020-01-03 20:03:54 -0300

    desafio68 - Jogo do par ou impar

--- Python-Mundo2-P2/desafio68.py
+++ Python-Mundo2-P2/desafio68.py
@@ -10,39 +10,24 @@ p = i = num = jogada = vitoria = derrota = 0
 computador = randint(0, 6)
 continuar = 'S'
 
-while continuar in 'sS':
-    num = int(input('Digite um numero :')[0])
-    acao = str(input('Voce quer par [p] ou impar[i] :'))
-    soma = computador + num
-    #  print(f'A soma de {num} e {computador} é {soma}')
+while True:
+    jogador = int(input('Digite um numero :')[0])
+    total = computador + jogador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Voce quer par [p] ou impar[i] :')).strip().upper()[0]
+        print(f'Voce jogou {jogador} e o computador jogou {computador}')
 
-    if soma % 2 == 0:
-        jogada += 1
-        if acao in 'Pp':
-            print(f'{soma} é par e voce Venceu ')
-            vitoria += 1
-        else:
-            print(f'{soma} é par e voce Perdeu ')
-            derrota +=1
-        continuar = str(input('Deseja Continuar [S] Sim e [N] Não :'))
-    if soma % 2 != 0:
-        jogada += 1
-        if acao in 'Pp':
-            print(f'{soma} é impar e voce Perdeu')
-            derrota += 1
-        else:
-            print(f' {soma} é impar e voce Venceu')
-            vitoria += 1
-        continuar = str(input('Deseja Continuar [S] Sim e [N] Não :'))
-    if continuar in 'nN':
-        print('*'*20)
-        print(f'\033[34mTotal de jogadas {jogada}\033[m\n\033[31mCOMPUTADOR: {derrota}\033[m\n\033[32mJOGADOR: {vitoria} \033[m')
-        print("FIM de JOGO")
-
-        if vitoria > derrota:
-            print('\033[32mPARABENS, VOCE É O VENCEDOR !!!\033[m')
-        elif vitoria == derrota:
-         print('\033[32mHOUVE EMPATE !!\033[m')
-        else:
-            print('\033[31mO COMPUTADOR GANHOU !!\033[m')
-        print('*' * 20)
+        if tipo == 'P':
+            if total % 2 == 0:
+               vitoria += 1
+               print('VOCE VENCEU!!')
+            else:
+                print('VOCE PERDEU!!')
+                break
+        elif tipo == 'I':
+            if total % 2 == 1:
+                print('VOCE VENCEU!!')
+            else:
+                print('VOCE PERDEU!!')
+    print('*' * 20)
