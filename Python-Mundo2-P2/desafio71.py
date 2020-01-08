f94605cc3bc5ff92f4ad087cc114b17b601d5961
Jeremias <ge39.medeiros@gmail.com>
2020-01-08 19:35:30 -0300

    desafio71 - Simulador caixa eletronico

--- Python-Mundo2-P2/desafio71.py
+++ Python-Mundo2-P2/desafio71.py
@@ -4,18 +4,20 @@
     e o programa irá informar quantas cedulas de cada valor serão entrgues
     OBS: considere que o caixa possui cedulas R$ 50,00, R$ 20,00, R$ 10,00, R$ 5,00 e R$1,00
 '''
-
+print('='*30)
+print('{:^30}'.format('BANCO CEV'))
+print('='*30)
 saque = int(input('Qual valor quer sacar ? \033[34mR$\033[m '))
 total = saque
 ced = 100
 totced  = 0
 while True:
-   if total >= ced:
+    if total >= ced:
        total -= ced
        totced += 1
-   else:
+    else:
         if totced > 0:
-          print(f'total de {totced} no valor de {ced}')
+          print(f'total de {totced} no valor de R${ced}')
         if ced == 100:
             ced = 50
         elif ced == 50:
@@ -29,7 +31,7 @@ while True:
         totced =0
         if total == 0:
             break
-            print('FIM')
+print('FIM')
 
 
 
