1c36ea4ef07fb7ccfefae6cb3fafbb36819ecc80
Jeremias <ge39.medeiros@gmail.com>
2020-01-23 18:59:28 -0300

    testefizzbuzz

--- Python-Mundo3/testefizzbuzz.py
+++ Python-Mundo3/testefizzbuzz.py
@@ -2,8 +2,11 @@ c1 = c2 = c3 =  0
 for c in range(1, 101):
     if c % 3 == 0:
       c = 'Fizz'
+      #print(f'{c}')
     elif c % 5 == 0:
-       c = 'Buzz'
-    elif c % 3 == c % 5:
-        c = 'Fizz Buzz'
-    print(f'{c}')
+     c = 'Buzz'
+     #print(c)
+    if(c % 5 == 1) == (c % 3 == 1):
+        print(c)
+    ##print(c)
+
