250107f7b1c755636c3d0f25f3d9d1bbcb9953a1
Jeremias <ge39.medeiros@gmail.com>
2020-01-23 20:32:51 -0300

    desafio74-maior e menor - valores em tupla

--- Python-Mundo3/desafio74-maior e menor - valores em tupla.py
+++ Python-Mundo3/desafio74-maior e menor - valores em tupla.py
@@ -1,5 +1,11 @@
 '''
     Crie um programa que vai gerar cinco numeros aleatórios e colocar em uma tupla
-    Depois disoo, mostre a listagem de numeros gerados e tambem indique
+    Depois disso, mostre a listagem de numeros gerados e tambem indique
     o menor eo maior valor que estão na tupla
 '''
+cont = 1
+from random import randint
+while cont <= 5:
+    lista = randint(10, 15)
+    cont += 1
+print(f'{lista}')
