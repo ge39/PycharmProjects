3f35cbb9407e84b05294e1d79d9485a82880c572
Jeremias <ge39.medeiros@gmail.com>
2020-01-23 18:56:24 -0300

    desafio74- Maior e menor com tuplas

---
+++ Python-Mundo3/desafio74-maior e menor - valores em tupla.py
@@ -0,0 +1,5 @@
+'''
+    Crie um programa que vai gerar cinco numeros aleatórios e colocar em uma tupla
+    Depois disoo, mostre a listagem de numeros gerados e tambem indique
+    o menor eo maior valor que estão na tupla
+'''
