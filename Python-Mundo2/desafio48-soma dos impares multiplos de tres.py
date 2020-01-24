5cfa071c6f9d6c310d97669566152d292f53ed68
Jeremias <ge39.medeiros@gmail.com>
2020-01-24 19:14:34 -0300

    desafio48-soma dos impares multiplos de tres

--- Python-Mundo2/desafio48-soma dos impares multiplos de tres.py
+++ Python-Mundo2/desafio48-soma dos impares multiplos de tres.py
@@ -1,7 +1,8 @@
 '''
     soma dos impares multiplos de 3
 '''
-
-for cont in range(1, 31):
-    if cont% 3 == 0:
-        print(cont, end=' ')
+soma = 0
+for cont in range(1, 500, 2):
+    if cont %3 == 0:
+        soma += cont
+print(soma, end=' ')
