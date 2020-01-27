dc370eb5c334a32f17f522dc27bec975056f3713
ge39 <ge39.medeiros@gmail.com>
2020-01-27 07:35:47 -0200

    desafio54-grupo da maioridade

--- Python-Mundo2/desafio54-grupo da maioridade.py
+++ Python-Mundo2/desafio54-grupo da maioridade.py
@@ -3,3 +3,17 @@
     No final , mostre Quantas pessoas qinda nao atingiram a maioridade e quantas já são maiores.
 
 '''
+
+from datetime import date
+atual = date.today().year
+q = 1
+maior = menor = 0
+for q in range(1, 8):
+    nasc = int(input(f'{q}-Digite o ano de nascimento :'))
+    if atual - nasc < 18:
+       menor += 1
+    else:
+        maior += 1
+print(f'Total de {menor}  pessoas menor de idade\nTotal de {maior} pessoas maior de 18 anos')
+
+
