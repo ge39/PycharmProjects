0ced8db92de998ce3ea19887d99aac9463a0dad5
ge39 <ge39.medeiros@gmail.com>
2020-01-27 07:37:52 -0200

    desafio54-grupo da maioridade

--- Python-Mundo2/desafio54-grupo da maioridade.py
+++ Python-Mundo2/desafio54-grupo da maioridade.py
@@ -10,10 +10,10 @@ q = 1
 maior = menor = 0
 for q in range(1, 8):
     nasc = int(input(f'{q}-Digite o ano de nascimento :'))
-    if atual - nasc < 18:
+    if atual - nasc < 21:
        menor += 1
     else:
         maior += 1
-print(f'Total de {menor}  pessoas menor de idade\nTotal de {maior} pessoas maior de 18 anos')
+print(f'Total de {menor} pessoas menor de idade\nTotal de {maior} pessoas maior de 18 anos')
 
 
