64249884bd3a8f958453e3c78bb8926fe0c84689
ge39 <ge39.medeiros@gmail.com>
2020-02-02 11:39:20 -0200

    Aula17-variaveis compostas(lista)parte01

--- Python-Mundo3/Aula17-variaveis compostas(lista)parte01.py
+++ Python-Mundo3/Aula17-variaveis compostas(lista)parte01.py
@@ -1,3 +1,24 @@
 '''
+    tupla = ()
+    lista = []
     Lista
 '''
+
+lanche = ['Hamburgeur','Suco', 'Pudim', 'Leite']
+lanche[3] = 'Chocolate'
+print(lanche)
+print('=' *40)
+print('\033[31mAPPEND\033[m - adiciona um elemento no final da fila')
+
+lanche.append('Queijo')
+print(lanche)
+
+print('='*40)
+print('\033[31mINSERT\033[m - adiciona um elemento em qualquer posicao da lista')
+lanche.insert(0,'Coco')
+print(lanche)
+
+print('='*40)
+print('\033[31mdel\033[m - apaga um elemento da lista pelo index')
+del lanche[3]
+print(lanche)
