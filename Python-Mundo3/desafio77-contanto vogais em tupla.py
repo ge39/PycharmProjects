4fa9e8ae5441b158584ee41c39cdfd57103e7c9f
ge39 <ge39.medeiros@gmail.com>
2020-02-02 08:59:44 -0200

    desafio77-contanto vogais em tupla

--- Python-Mundo3/desafio77-contanto vogais em tupla.py
+++ Python-Mundo3/desafio77-contanto vogais em tupla.py
@@ -4,10 +4,16 @@
     Depois disso, voce deve mostrar, para cada palavra, quais sao as suas vogais
 '''
 
-palavras = ('java script', 'Python', 'Php', 'Java', 'Ruby', 'Html', 'Mysql')
+palavras = ('java script', 'Python', 'Php', 'Java', 'Ruby', 'Html', 'Mysql', 'django', 'Git', 'Wordpress')
 
-for p in palavras:
-    print(f'\nNa palavra {p.upper()} temos as vogais ', end='')
-    for letra in p:
-      if letra.lower() in 'aeiou':
-        print(f'\033[34m{letra}\033[m', end=' ')
+    # percorre a lista de palavras e armazena na variavel "lista"
+for lista in palavras :
+    print(f'\nNa palavra {lista.upper()} tem ', end=' ')
+
+    #percorre os dados da variavel lista e armazena em "vogal"
+    for vogal in lista:
+        if vogal in 'aeiou':
+            print(f'\033[32m{vogal.upper()}\033[m', end=' ')
+
+    #identifica e separa os valores que estão armazenados na variavel letra
+
