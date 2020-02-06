b936967d4683915f288e938d4d618be7bf4b1921
Jeremias <ge39.medeiros@gmail.com>
2020-02-06 19:13:41 -0300

    desafio83-validando expressoes matematicas

---
+++ Python-Mundo3/desafio83-validando expressoes matematicas.py
@@ -0,0 +1,5 @@
+'''
+    Crie um programa onde o usuario digite uma expressao qualquer
+    que use parenteses.
+    Seu aplicativo deverá analisar se a expressao passada está com os parenteses abertos e fechados na ordem correta
+'''
