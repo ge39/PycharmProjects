f30b294aed976f4ce929beb3f4081a461d54dc74
Jeremias <ge39.medeiros@gmail.com>
2020-02-06 18:43:09 -0300

    desafio79-valores unico em uma lista

---
+++ Python-Mundo3/desafio79-valores unico em uma lista.py
@@ -0,0 +1,6 @@
+'''
+    crie um programa, onde o usuario possa digitar varios valores numericos
+    e cadastre-os em uma lista. Caso o numero já exista lá dentro.
+    Ele não será adicionado.
+    No final, serão exibidos todos os valores unicos digitados em ordem crescente
+'''
