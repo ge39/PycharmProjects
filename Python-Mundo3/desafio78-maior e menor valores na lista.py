d8b27eda268f682e53eb61f41a0ec671a3c9a7da
Jeremias <ge39.medeiros@gmail.com>
2020-02-12 18:56:45 -0300

    desafio78-maior e menor valores na lista

--- Python-Mundo3/desafio78-maior e menor valores na lista.py
+++ Python-Mundo3/desafio78-maior e menor valores na lista.py
@@ -3,21 +3,17 @@
   No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições
 '''
 numero = maior = menor = 0
-pmaior = pmenor = 0
-valor = (0, 1, 2, 3, 4)
-for pos, num in enumerate(valor):
-    numero = int(input(f'Digite um numero na posição {num}:'))
-    if num == 0:
-        maior = numero
-        menor = numero
-    else:
-        if numero >= maior:
-            maior = numero
-            pmaior = num
-            pmaior = pos
-        elif numero <= menor:
-            menor = numero
-            pmenor = pos
-print(f'\033[31mO numero maior é {maior} na posição {pmaior}\033[m\n\033[33mO numero menor é {menor} na posição {pmenor}\033[m')
 
+listanum = []
+
+for c in range(0, 5):
+    listanum.append(int(input('Digite um Valor: ')))
+if c == 0:
+       maior = menor = listanum[c]
+else:
+   if listanum[c] > maior:
+       maior = listanum[c]
+   if listanum[c] < menor:
+        menor = listanum[c]
+print(f'O Menor valor da lista é {menor}/nO maior valor é {maior}')
 
