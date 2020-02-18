abbe12edce59fa888517fe1533857d607b06ed9d
ge39 <ge39.medeiros@gmail.com>
2020-02-18 07:13:09 -0300

    desafio78-maior e menor valores na lista

--- Python-Mundo3/desafio78-maior e menor valores na lista.py
+++ Python-Mundo3/desafio78-maior e menor valores na lista.py
@@ -6,18 +6,27 @@ c = pos = maior = menor = 0
 listanum = []
 for c in range(0, 5):
     listanum.append(int(input(f'Digite um Valor na posição {c}: ')))
-    for pos in enumerate(listanum[c]):
-        if c == 0:#  Se for inicio do laço faça ....
-            maior = listanum[c]
+    if c == 0:#  Se for inicio do laço faça ....
+        maior = listanum[c]
+        menor = listanum[c]
+    else:# Se não for o inicio do laço, então ......
+        if listanum[c] > maior:
+           maior = listanum[c]
+        if listanum[c] < menor:
             menor = listanum[c]
-        else:# Se não for o inicio do laço, então ......
-            if listanum[c] > maior:
-               maior = listanum[c]
-            if listanum[c] < menor:
-                menor = listanum[c]
 print(f'Lista total {listanum}')
-for i, v in enumerate(listanum):
-    if v == menor:
+print(f'O Menor valor da lista é o : {menor} na posicao ', end=' ')
 
-print(f'O Menor valor da lista é : {menor} na posicao {pos}\nO maior valor da lista é : {maior} na posição{pos}')
+for indice, valor in enumerate(listanum):
+    if valor == menor:
+        print(f' {indice}...', end='')
+print()
+print(f'O Maior valor da lista é o : {maior} na posição', end='')
+for indice, valor in enumerate(listanum):
+    if valor == maior:
+       print(f' {indice}...', end='')
+print()
+
+
+
 
