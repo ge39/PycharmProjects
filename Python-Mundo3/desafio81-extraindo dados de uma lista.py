4debd611dcc62550b029c4d286b8edf4a9dafcc9
ge39 <ge39.medeiros@gmail.com>
2020-04-14 21:23:32 -0300

    desafio81-extraindo dados de uma lista

--- Python-Mundo3/desafio81-extraindo dados de uma lista.py
+++ Python-Mundo3/desafio81-extraindo dados de uma lista.py
@@ -1,8 +1,26 @@
 '''
-    Crie um programa que leia varios numeros e coloque um uma
+    Crie um programa que leia varios numeros e coloque em uma lista
     depois disso mostre:
 
     A - Quantos numeros foram digitados
     B - A lista de valores ordenados de forma decrescente
     C - Se o valor 5 foi digitado e está ou não na lista
 '''
+from itertools import count
+
+lista = []
+cont = 0
+num = 1
+while num != 0:
+    num = int(input("Digite um numero :"))
+    lista.append(num)
+    cont += 1
+
+print(f'Total de numeros digitados: {cont} numeros')
+lista.sort(reverse=True)
+print(f'Lista de valores decrescente {lista}')
+
+if 5 in lista:
+    print(f'O numero 5 aparece na lista {lista.count(5)} vezes')
+else:
+    print('O numero 5 nao aparece na lista')
