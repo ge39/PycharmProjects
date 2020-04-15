0c0ff3e062f96b3c493b52fd46976e5bcf7f898a
ge39 <ge39.medeiros@gmail.com>
2020-04-14 22:47:45 -0300

    desafio85-listas com pares e impares

---
+++ Python-Mundo3/desafio85-listas com pares e impares.py
@@ -0,0 +1,5 @@
+""""
+Crie um programa que o usuario possa digitar 7 valores numericos
+e cadastre-os em uma lista unica que mantenha separado os valores pares e impares
+No final mostre os valores pares e impares em ordem crescente
+"""
