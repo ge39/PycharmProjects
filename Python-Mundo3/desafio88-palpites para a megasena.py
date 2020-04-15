d91e241b1db155adc91f5cc53794f0c99808d0ee
ge39 <ge39.medeiros@gmail.com>
2020-04-14 23:04:48 -0300

    desafio88-palpites para a megasena

---
+++ Python-Mundo3/desafio88-palpites para a megasena.py
@@ -0,0 +1,5 @@
+"""
+Faca um programa que ajude o jogador da mega sena criar palpites,
+O programa vai perguntar quantos jogos serao gerados
+e vai sortear 6 numeros entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta
+"""
