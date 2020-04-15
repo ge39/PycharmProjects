eadf5e2330a3051d1d6c704c9eaf43d2e600e181
ge39 <ge39.medeiros@gmail.com>
2020-04-14 22:38:06 -0300

    desafio84-lista composta e analise de dados

--- Python-Mundo3/desafio84-lista composta e analise de dados.py
+++ Python-Mundo3/desafio84-lista composta e analise de dados.py
@@ -1,4 +1,9 @@
 """
+Faca um programa que leia o nome e peso de varias pessoas,
+guardando tudo em uma lista e no final mostre:
 
+A - Quantas pessoas foram cadastradas
+B - Uma listagem com as pessoas mais pesadas
+C - Uma listagem com as pessoas mais leve
 
 """
