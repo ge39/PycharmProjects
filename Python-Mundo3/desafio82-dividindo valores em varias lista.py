d5f92a475f1bab19daa99bcad985a0f0169cc036
ge39 <ge39.medeiros@gmail.com>
2020-04-14 21:37:41 -0300

    desafio82-dividindo valores em varias lista

--- Python-Mundo3/desafio82-dividindo valores em varias lista.py
+++ Python-Mundo3/desafio82-dividindo valores em varias lista.py
@@ -1,6 +1,13 @@
 '''
     Crie um programa que vai ler varios numeros e colocar em uma lista
-    Depois disso, Crie duas listas extras que vao conter apenas os valores pares
+    Depois disso,
+    Crie duas listas extras que vao conter apenas os valores pares
     e os valores impares digitados, respectivamente.
     Ao final mostre o conteudo das 3 listas geradas
 '''
+lista = par = impar = []
+n = 1
+while n != 0:
+    n = int(input("Digite um valor :"))
+    lista.append(n)
+print(f'Lista geral dos valores {lista}')
