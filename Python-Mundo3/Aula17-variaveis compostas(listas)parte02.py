9c90fe31a017e2064d6493b90d79dcad47b85d8b
ge39 <ge39.medeiros@gmail.com>
2020-04-16 19:19:31 -0300

    Aula17-variaveis compostas(lista)parte02

--- Python-Mundo3/Aula17-variaveis compostas(listas)parte02.py
+++ Python-Mundo3/Aula17-variaveis compostas(listas)parte02.py
@@ -42,10 +42,24 @@ print(f'  {pessoa[1][:]}')
 print("")
 print("  \033[33mListando todos os dados da estrutura lista Pessoa \n  trazendo todos os dados não definindo o index\033[m")
 print(f'  {pessoa}')
+print('='*40)
 
-teste = []
-teste.append('Gustavo')
-teste.append(30)
 galera = []
-galera.append(teste[:])
-print(galera)
+dado = []
+totmaior = totmenor = 0
+for c in range(0, 3):
+    dado.append(str(input("Digite um nome: ").upper()))
+    dado.append(int(input("Idade: ")))
+    galera.append(dado[:])
+    dado.clear()
+
+for p in galera:
+    if p[1] >= 21:
+        totmaior +=1
+        print(f'{p[0]} é maior de idade e tem {p[1]} anos')
+    else:
+        totmenor +=1
+        print(f'{p[0]} é menor de idade e tem {p[1]} anos')
+print(f'\033[33mTotal maior de idade: {totmaior} pessoas\nTotal menor de idade: {totmenor} pessoas\033[m')
+
+
