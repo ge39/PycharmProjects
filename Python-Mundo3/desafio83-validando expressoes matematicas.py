5f2fcbb6467c622134249c65120d515d3aa4df88
ge39 <ge39.medeiros@gmail.com>
2020-04-16 18:07:20 -0300

    desafio83-validando expressoes matematicas

--- Python-Mundo3/desafio83-validando expressoes matematicas.py
+++ Python-Mundo3/desafio83-validando expressoes matematicas.py
@@ -4,3 +4,21 @@
     Seu aplicativo deverá analisar se a expressao passada está com
     os parenteses abertos e fechados na ordem correta
 '''
+
+expr = str(input("Digite uma expressao"))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ")":
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print("Sua expressao está correta")
+else:
+    print("Sua expressao está incorreta")
+
+
