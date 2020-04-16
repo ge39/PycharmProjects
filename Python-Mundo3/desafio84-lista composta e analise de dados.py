cb4cf9c679155e3ea55747e0178ab387b5311de7
ge39 <ge39.medeiros@gmail.com>
2020-04-16 19:20:11 -0300

    desafio84-lista composta e analise de dados

--- Python-Mundo3/desafio84-lista composta e analise de dados.py
+++ Python-Mundo3/desafio84-lista composta e analise de dados.py
@@ -8,6 +8,7 @@ C - Uma listagem com as pessoas mais leve
 
 """
 pessoa = []
+pesopessoa = []
 lista = []
 cont = 0
 while True:
@@ -18,10 +19,12 @@ while True:
     lista.append(pessoa[:])
     resp = str(input("Quer continuar: ")).upper()
     cont+=1
-
+    if peso  < peso:
+        pesopessoa.append(peso)
+        print(f'Maior peso {pesopessoa}')
     if resp not in "sS":
         break
-print(f' Total de pessoas cadastradas: {cont} pessoas')
+print(f'Total de pessoas cadastradas: {cont} pessoas')
 # print(f'O maior peso foi {mais} e o menor foi {menos}')
 print('=-' * 30)
 
