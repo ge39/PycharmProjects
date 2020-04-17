252699efdf1d5b78da196ebde5edf7b68a2eee49
ge39 <ge39.medeiros@gmail.com>
2020-04-17 20:27:31 -0300

    desafio85-listas com pares e impares

--- Python-Mundo3/desafio85-listas com pares e impares.py
+++ Python-Mundo3/desafio85-listas com pares e impares.py
@@ -3,16 +3,18 @@ Crie um programa que o usuario possa digitar 7 valores numericos
 e cadastre-os em uma lista unica que mantenha separado os valores pares e impares
 No final mostre os valores pares e impares em ordem crescente
 """
-num = []
+# Lista e sublistas
+num = [[], []]
 for v in range(0, 7):
-    num.append(float(input(f"Digite o {v + 1}º numero: ")))
-print(num)
-
-for p in num:
-    if p % 2 == 0:
-       print(f'par - {p}\n ', end='')
-
+    valor = float(input(f"Digite o {v + 1}º numero: "))
+    if valor % 2 == 0:
+        num[0].append(valor)
     else:
-        print(f'impar - {p}\n ', end='')
-
+        num[1].append(valor)
+print('-=' * 40)
+num[0].sort()
+num[1].sort()
+print(f'\033[34mTotal de {len(num[0] + num[1])} numeros digitados\033[m')
+print(f'Os valores pares sao {num[0]}')
+print(f'Os valores impares sao {num[1]}')
 
