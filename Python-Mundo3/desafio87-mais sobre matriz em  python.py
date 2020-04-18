0c0480f66f260b34cc26bd8c1bbcc88eca2a5680
ge39 <ge39.medeiros@gmail.com>
2020-04-18 18:56:17 -0300

    desafio87-mais sobre matriz em python

--- Python-Mundo3/desafio87-mais sobre matriz em  python.py
+++ Python-Mundo3/desafio87-mais sobre matriz em  python.py
@@ -2,5 +2,32 @@
 Aprimore o desafio anterior, mostrando no final
 A - A soma de todos os valores pares
 B - A soma dos valores da terceira coluna
-C - O maior valor da segunada linha
+C - O maior valor da segunda linha
  """
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+scol = spar = 0
+for l in range(0, 3):
+    for c in range(0, 3):
+       matriz[l][c] = int(input(f'Digite [{l}, {c}] :'))
+print('=-' *40)
+for l in range(0, 3):
+    for c in range(0, 3):
+       print(f'[{matriz[l][c]:^5}]', end='')
+       if matriz[l][c] % 2 == 0:
+           spar += matriz[l][c]
+    print()
+print('-=' *40)
+print(f'Soma dos valores pares é {spar}.', end='')
+for l in range(0, 3):
+    scol += matriz[l][2]
+print(f'\nA soma dos valores da 3ª coluna é {scol}.')
+
+for c in range(0, 3):
+    if c == 0:
+        maior = matriz[1][c]
+    elif matriz[1][c] > maior:
+        maior = matriz[1][c]
+print(f'O maior valor da 2ª linha é {maior}.')
+
+
