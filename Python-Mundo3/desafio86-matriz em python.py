cabf41f63d6096eb17c4df22aa75e60a59e97620
ge39 <ge39.medeiros@gmail.com>
2020-04-18 08:57:57 -0300

    desafio86-matriz em python

--- Python-Mundo3/desafio86-matriz em python.py
+++ Python-Mundo3/desafio86-matriz em python.py
@@ -3,3 +3,12 @@ Crie um programa que crie uma matriz de dimensao 3x3 e preencha com valores lido
 No final, mostre a matriz na tela com a formatação correta
 
 """
+matriz= [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+for c in range(0, 3):
+     for l in range(0, 3):
+        matriz[l][c] = int(input(f'Digite o valor [{l}, {c}]:'))
+
+print(matriz, end='')
+print()
+
