92e5bdae557f2b26f06a93d84c2edd000471d161
ge39 <ge39.medeiros@gmail.com>
2020-04-26 08:47:13 -0300

    desafio90-dicionario em python.py

--- Python-Mundo3/desafio90-dicionario em python.py
+++ Python-Mundo3/desafio90-dicionario em python.py
@@ -3,3 +3,17 @@ Faça um programa que leia nome e média de um aluno.
 guardando tambem a situação em um dicionario.
 No final, mostre o conteudo da estrutura na tela
 '''
+dados = {}
+
+while True:
+    dados['nome'] = str(input('Nome: ').upper())
+    dados['media'] = float(input('Média: '))
+
+    if dados['media'] >= 5:
+        dados['situacao'] = 'Aprovado'
+        #print(f'Aluno Aprovado')
+        print(dados)
+    else:
+        dados['situacao'] = 'Reprovado'
+       #
+        print(dados)
