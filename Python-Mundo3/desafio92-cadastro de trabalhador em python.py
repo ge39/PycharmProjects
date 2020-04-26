41ba009a74cb8c209b29a0f865ffce30c66c8b2f
ge39 <ge39.medeiros@gmail.com>
2020-04-26 07:04:07 -0300

    desafio92-cadastro de trabalhador em python

---
+++ Python-Mundo3/desafio92-cadastro de trabalhador em python.py
@@ -0,0 +1,5 @@
+'''
+Crie um programa que leia: nome, ano de nascimento e carteira de trabalho e cadsastre-os com (idade)
+em um dicionario se por acaso a ctps for diferente de zero, o dicionario receberá tambem o ano de contratação
+e o salario.Calcule e acrescente, alem, da idade, com quantos anos a pessoa vai se aposentar
+'''
