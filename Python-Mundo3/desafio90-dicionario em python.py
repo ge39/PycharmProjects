49725e8d74af3bb8cc3c99a76d9a042583c02d04
ge39 <ge39.medeiros@gmail.com>
2020-04-27 08:16:27 -0300

    desafio90-dicionario em python

--- Python-Mundo3/desafio90-dicionario em python.py
+++ Python-Mundo3/desafio90-dicionario em python.py
@@ -3,16 +3,24 @@ Faça um programa que leia nome e média de um aluno.
 guardando tambem a situação em um dicionario.
 No final, mostre o conteudo da estrutura na tela
 '''
-dados = {}
+aluno = {}
 
-while True:
-    dados['nome'] = str(input('Nome: ').upper())
-    dados['media'] = float(input('Média: '))
 
-    if dados['media'] >= 5:
-        dados['situacao'] = 'Aprovado'
-        #print(f'Aluno Aprovado')
-        print(dados)
-    else:
-        dados['situacao'] = 'Reprovado'
-        print(dados)
+aluno['nome'] = str(input('Nome: ').upper())
+aluno['media'] = float(input(f'Média {aluno["nome"]}: '))
+
+
+if aluno['media'] >= 5:
+    aluno['situacao'] = 'Aprovado'
+    #print(f'Aluno Aprovado')
+elif 4 <= aluno['media'] < 5:
+    aluno['situacao'] = 'Recuperacao'
+else:
+    aluno['situacao'] = 'Reprovado'
+print('-='*30)
+for k, v in aluno.items():
+     if aluno['situacao'] == 'Reprovado':
+        print(f'{k} é igual \033[31m{v}\033[m')
+     else:
+         print(f'{k} é igual \033[34m{v}\033[m')
+
