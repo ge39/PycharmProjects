5f4a78c1406f95e8a1d5b0b374710ac1b52364cd
ge39 <ge39.medeiros@gmail.com>
2020-04-27 08:48:55 -0300

    desafio91-jogo de dados em python

--- Python-Mundo3/desafio91-jogo de dados em python.py
+++ Python-Mundo3/desafio91-jogo de dados em python.py
@@ -4,12 +4,19 @@
     No final , coloque esse dicionario em ordem,sabendo que o vencedor tirou o maior numero no dado
 '''
 from random import randint
-analise = dict()
+from time import sleep
+from operator import itemgetter
 
-for j in range(0, 4):
-    analise['player'] = j
-    # jogadas do dado = 6
-    num = randint(1, 7)
-    analise['point'] = num
-    print(analise)
-
+jogo = { 'jogador1' : randint(1,6),
+         'jogador2' : randint(1,6),
+         'jogador3' : randint(1,6),
+         'jogador4' : randint(1,6)}
+ranking = list()
+for k, v in jogo.items():
+  print(f'{k} tirou {v} no dado.')
+  sleep(1)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+#print(ranking)
+print('Valores sorteados')
+for i, v in enumerate(ranking):
+    print(f'{i+1}º lugar: {v[0]} com {v[1]}.')
