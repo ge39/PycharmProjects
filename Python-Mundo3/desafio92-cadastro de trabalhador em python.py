0530af29f0ab31b110d26f7e2357fe6337874e5b
ge39 <ge39.medeiros@gmail.com>
2020-04-28 08:23:49 -0300

    desafio92-cadastro de trabalhador em python

--- Python-Mundo3/desafio92-cadastro de trabalhador em python.py
+++ Python-Mundo3/desafio92-cadastro de trabalhador em python.py
@@ -3,3 +3,16 @@ Crie um programa que leia: nome, ano de nascimento e carteira de trabalho e cads
 em um dicionario se por acaso a ctps for diferente de zero, o dicionario receberá tambem o ano de contratação
 e o salario.Calcule e acrescente, alem, da idade, com quantos anos a pessoa vai se aposentar
 '''
+from datetime import date
+ficha = dict()
+ano_atual = date.today().year
+ficha['nome'] = str(input('Nome:').upper())
+nascido=int(input('Nascido em: '))
+ficha['idade'] = ano_atual - nascido
+ficha['ctps'] = int(input('Carteira Prof [0] nao possui: '))
+if ficha['ctps'] != 0:
+  ficha['contrato'] = int(input('Ano Contrato: '))
+  ficha['salario'] = float(input('Salário: R$'))
+  ficha['aposentadoria'] = ficha['contrato'] + 35
+for k, v in ficha.items():
+    print(f'- {k} tem o valor {v}')
