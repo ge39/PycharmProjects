33ba57e19e0ae98fe86ee4e7ac5d836bb5ace182
ge39 <ge39.medeiros@gmail.com>
2020-04-28 08:37:01 -0300

    desafio92-cadastro de trabalhador em python

--- Python-Mundo3/desafio92-cadastro de trabalhador em python.py
+++ Python-Mundo3/desafio92-cadastro de trabalhador em python.py
@@ -4,15 +4,16 @@ em um dicionario se por acaso a ctps for diferente de zero, o dicionario receber
 e o salario.Calcule e acrescente, alem, da idade, com quantos anos a pessoa vai se aposentar
 '''
 from datetime import date
-ficha = dict()
+dados = dict()
 ano_atual = date.today().year
-ficha['nome'] = str(input('Nome:').upper())
+dados['nome'] = str(input('Nome: ').upper())
 nascido=int(input('Nascido em: '))
-ficha['idade'] = ano_atual - nascido
-ficha['ctps'] = int(input('Carteira Prof [0] nao possui: '))
-if ficha['ctps'] != 0:
-  ficha['contrato'] = int(input('Ano Contrato: '))
-  ficha['salario'] = float(input('Salário: R$'))
-  ficha['aposentadoria'] = ficha['contrato'] + 35
-for k, v in ficha.items():
-    print(f'- {k} tem o valor {v}')
+dados['idade'] = ano_atual - nascido
+dados['ctps'] = int(input('Carteira Prof [0] nao possui: '))
+if dados['ctps'] != 0:
+  dados['contrato'] = int(input('Ano Contrato: '))
+  dados['salario'] = float(input('Salário: R$ '))
+  dados['aposentadoria'] = dados['idade'] + ((dados['contrato'] + 35) - ano_atual)
+
+for k, v in dados.items():
+    print(f'- {k} tem o valor \033[34m{v}\033[m')
